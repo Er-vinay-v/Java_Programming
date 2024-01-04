50e10c649265d8abbed5ee93bdfad9de470d5c6c
Vinay Verma <151910988+Er-vinay-v@users.noreply.github.com>
2024-01-04 23:33:39 +0530

    Add files via upload

---
+++ Question_practice/calculator.java
@@ -0,0 +1,14 @@
+package Question_practice;
+import java.util.*;
+public class calculator {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the integer string you want to calculate");
+        String s=sc.nextLine();
+        Stack<String>st=new Stack<>();
+       for (int i=0;i<s.length();i++){
+
+       }
+
+    }
+}
